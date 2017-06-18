24ab644162c2657019b9302e2b09944864903952
aisensiy <aisensiy@163.com>
2017-06-18 14:30:27 +0800

    basic config files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+	module: {
+    rules: [
+      {
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"
+      },
+      {
+        test: /\.css$/, loader: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+      },
+      {
+        test: /\.jpg|png$/, loader: 'file-loader'
+      }
+    ]
+	},
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'})
+  ]
+};
