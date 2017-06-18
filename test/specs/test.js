510401a779f9d43019ab6bb0890b816630465727
aisensiy <aisensiy@163.com>
2017-06-18 17:32:06 +0800

    update configuration

---
+++ test/specs/test.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'Should create a new todo item': function(browser) {
+        browser
+            .url(browser.launch_url)
+            .waitForElementVisible('body', 500)
+            .setValue('input.new-todo', ['new todo', browser.Keys.ENTER])
+            .pause(500)
+            .assert.containsText('.view', 'new todo');
+    },
+
+    'Should click the new todo and mark it as finished': function(browser) {
+        browser
+            .click('.view .toggle')
+            .pause(500)
+            .assert.cssClassPresent('li[data-id]', 'completed')
+            .click('.view .toggle')
+            .pause(500)
+            .assert.cssClassNotPresent('li[data-id]', 'completed');
+    },
+
+    'Should show current todo count': function(browser) {
+        browser
+            .assert.containsText('footer .todo-count', '1');
+    },
+
+    'Should remove the todo item': function(browser) {
+        browser
+            .assert.elementPresent('.todo-list li')
+            .click('.todo-list li:first-child .destroy')
+            .pause(500)
+            .assert.elementNotPresent('.todo-list li');
+    }
+}
